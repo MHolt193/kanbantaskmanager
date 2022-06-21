34f89e5f1b049148390f1db68db0a50800eeb712
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-06-21 10:52:54 -0600

    installed axios and started login functionality

--- src/Components/LoginSignup/Login.js
+++ src/Components/LoginSignup/Login.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 const Login = (props) =>{
+
+    const loginSubmitHandler = async (event) =>{
+        event.preventDefault();
+        const form = event.target
+        const formData = {
+            email: form.email.value,
+            password: form.password.value
+        }
+        console.log(formData);
+    }
+
     return(
         <div>
-            <form>
-                <input type='email' placeholder='Email' />
-                <input type='password' placeholder='Password' />
+            <form onSubmit={loginSubmitHandler}>
+                <input type='email' placeholder='Email' name='email' />
+                <input type='password' placeholder='Password' name='password' />
                 <button type='submit'>Login</button>
                 <p>Need to register? <Link to='/signup'>Register</Link></p> 
             </form>
