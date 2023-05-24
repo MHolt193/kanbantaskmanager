ef8fbc73980d4c04d2e51514c15c33742221db4b
MHolt193 <michael.holt.ah@gmail.com>
2023-05-24 09:35:34 -0600

    added dropdown to notification component

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import icon from "./211694_bell_icon.svg"
 import classes from "./Notifications.module.css"
 
 const Notifications = () =>{
+  //State
+  const [dropdownActive, setDropdownActive] = useState(false);
+  
+  //Habdlers
+  let iconClickHandler = (e) =>{
+    setDropdownActive((prev)=> !prev)
+  }
+  
   return <div>
- <img className={classes.icon} src={icon} alt=""/>
+ <img onClick={iconClickHandler} className={classes.icon} src={icon} alt=""/>
+ <div className={dropdownActive ? `${classes.dropdown} ${classes.active}`:`${classes.dropdown} ${classes.inactive}`}>
+ <ul>
+ <li>Notification 1</li>
+ </ul>
+ </div>
   </div>
 }
 
